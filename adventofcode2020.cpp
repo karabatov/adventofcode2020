7b85a6edd14a11a95525671498d16a48009d23e9
Yuri Karabatov <yuri@yurikarabatov.com>
2020-12-01 22:44:42 +0300

    Add project files.

---
+++ adventofcode2020.cpp
@@ -0,0 +1,12 @@
+﻿// adventofcode2020.cpp : Defines the entry point for the application.
+//
+
+#include "adventofcode2020.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
