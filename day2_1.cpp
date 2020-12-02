73a748e999b8b8a5bf93ca82e537875f3d3b2fec
Yuri Karabatov <yuri@yurikarabatov.com>
2020-12-02 23:31:48 +0300

    Solve day 2 part 1

---
+++ day2_1.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+// https://adventofcode.com/2020/day/2
+
+int solve(std::fstream &in) {
+  int valid{0};
+  std::string line;
+  int lo{0}, hi{0};
+  char letter{0};
+  std::string pass;
+  while (getline(in, line)) {
+    std::stringstream ss(line);
+    ss >> lo;
+    ss.ignore(1, '-');
+    ss >> hi;
+    ss.ignore(1, ' ');
+    ss >> letter;
+    ss.ignore(1, ':');
+    ss.ignore(1, ' ');
+    ss >> pass;
+
+    int count{0};
+    for (auto c : pass) {
+      if (c == letter) count++;
+    }
+    if (count >= lo && count <= hi) valid++;
+  }
+  return valid;
+}
+
+int main() {
+  std::fstream in_file;
+  in_file.open("input/day2.txt", std::ios::in);
+  if (in_file.is_open()) {
+    std::cout << solve(in_file) << std::endl;
+  }
+  in_file.close();
+}
